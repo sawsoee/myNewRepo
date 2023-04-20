5560ab9c469fc1380f6f393ccee86087e8bea2df
sawsoee <sawsoer1@gmail.com>
2023-04-20 10:10:42 +0300

    m3

---
+++ test/src/App.java
@@ -0,0 +1,11 @@
+import java.util.function.Function;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+
+    }
+    
+    
+}
